1c38f53621a98efbd85555ac832aab70414b19d8
Daniel Faria <danielfaria18@gmail.com>
2016-02-13 21:48:05 -0200

    Modelo Criando, mas ao executar o comando de migração dá falaha no sqllite

--- api/models.py
+++ api/models.py
@@ -1,5 +1,49 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Provedor(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    nome  = models.CharField(max_length=100, blank=False)
+    email = models.CharField(max_length=100, blank=True, default='')
+    site  = models.CharField(max_length=100, blank=False)
+
+    class Meta:
+        ordering = ('created',)
+
+class SistemaOperacional(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    nome  = models.CharField(max_length=100, blank=False)
+    class Meta:
+        ordering = ('created',)	
+
+
+class Pacote(models.Model):
+    created       = models.DateTimeField(auto_now_add=True)
+    nome          = models.CharField(max_length=100, blank=False)
+
+    quantidadeCpu = models.IntegerField(validators=[MinValueValidator(1),
+                                       MaxValueValidator(1000)])
+    memoria       = models.IntegerField(validators=[MinValueValidator(1),
+                                       MaxValueValidator(1000)])
+
+    tamanhoDisco  = models.IntegerField(validators=[MinValueValidator(1),
+                                       MaxValueValidator(1000)])
+
+    valorHora     = models.DecimalField(max_digits=12, decimal_places=2,  default=0.0)
+
+    #Chaves  estrangeiras 
+    sistemaOperacional = models.ForeignKey(SistemaOperacional, on_delete=models.CASCADE)
+    provedor           = models.ForeignKey(Provedor, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('created',)
+    
+
+
+
+
+
+  
