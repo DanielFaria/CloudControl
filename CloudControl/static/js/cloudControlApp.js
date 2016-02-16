602335c4b22fe8f435d6b1694fd565f29d318104
Daniel Faria <danielfaria18@gmail.com>
2016-02-16 02:30:17 -0200

    Cadastros de Provedores Funcionando

---
+++ CloudControl/static/js/cloudControlApp.js
@@ -0,0 +1,81 @@
+var profileEditApp = angular.module('cloudControlApp', []);
+ 
+profileEditApp.config(['$httpProvider', '$interpolateProvider',
+    function($httpProvider, $interpolateProvider) {
+    /* for compatibility with django teplate engine */
+    $interpolateProvider.startSymbol('{$');
+    $interpolateProvider.endSymbol('$}');
+    /* csrf */
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+}]);
+ 
+profileEditApp.controller('cloudControlCtrl', function ($scope, $http) {
+    
+
+    $scope.selecionaForm  =  function(idForm) {
+      $scope.form = idForm;
+      $scope.data = {};
+    }  
+  
+    $scope.getSistemas = function() {
+        $scope.sistemas =  [];
+        $http({method: 'GET', url: 'sistemas'}).
+            success(function(data, status, headers, config) {
+                $scope.sistemas = data;
+        }) 
+    }
+
+    $scope.getProvedores = function() {
+        $scope.provedores =  [];
+        $http({method: 'GET', url: 'provedores'}).
+            success(function(data, status, headers, config) {
+                $scope.provedores = data;
+        }) 
+    }
+    $scope.atualizaOuSalvaPacotes = function() {
+        $http({method: 'POST', url: 'pacotes/',
+           data: { nome: $scope.data['nomePacote'],
+                   quantidadeCpu : $scope.data['quantidadeCpu'],
+                   memoria : $scope.data['quantidadeMemoria'],
+                   tamanhoDisco :$scope.data['quantidadeDisco'],
+                   valorHora: $scope.data['valorHora'],
+                   sistemaOperacional_id : $scope.data['so'],
+                   provedor_id           : $scope.data['provedor']
+          }}).success(function(data, status, headers, config) {
+            $scope.data['success'] = true;
+        }).
+        error(function(data, status, headers, config) {
+            $scope.data['fail'] = true;
+    }); 
+  }
+  $scope.atualizaOuSalvaProvedores = function() {
+        $http({method: 'POST', url: 'provedores/',
+           data: { nome: $scope.data['nomeProvedor'],
+                   site : $scope.data['emailProvedor'],
+                   email : $scope.data['siteProvedor']
+          }}).success(function(data, status, headers, config) {
+            $scope.data['success'] = true;
+        }).
+        error(function(data, status, headers, config) {
+            $scope.data['fail'] = true;
+    }); 
+  }  
+   
+   $scope.atualizaOuSalvaSistemas = function() {
+        $http({method: 'POST', url: 'sistemas/',
+           data: { nome: $scope.data['nomeSO'],
+          }}).success(function(data, status, headers, config) {
+            $scope.data['success'] = true;
+        }).
+        error(function(data, status, headers, config) {
+            $scope.data['fail'] = true;
+    }); 
+  }
+
+    
+
+  $scope.form=1;
+
+
+});
